5001a7d9e1e71cc5993195bb82b441b939cb205d
sunyan <sunyan@migu.cn>
2022-03-07 09:51:50 +0800

    配置flexlibe

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, Redirect } from 'react-router-dom';
-import routerList from './router/index';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import routerList from './router/index';
 import Home from './view/home';
 import Card from './view/card';
 import NavBottom from './components/navBottom'
 import "./index.css";
 
 export default function App() {
-  const name = "John Doe";
   return (
     <Router>
-      {/* <main>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">首页</Link>
-            </li>
-            <li>
-              <Link to="/card">银行卡</Link>
-            </li>
-          </ul>
-        </nav>
-        
-      </main> */}
       <NavBottom></NavBottom>
       <Routes>
             <Route path="/" element={<Home />}></Route>
--- src/view/card/index.jsx
+++ src/view/card/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Card extends React.Component {
     render() {
         return (
-            <div>银行卡</div>
+            <div>我是银行卡</div>
         )
     }
 }

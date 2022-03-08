1b33dcde3c998d204f113ceab924c8afad3c00bf
sunyan <sunyan@migu.cn>
2022-03-08 10:00:36 +0800

    顶部导航

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 // import routerList from './router/index';
 import Home from './view/home';
-import Card from './view/card';
-import NavBottom from './components/navBottom'
-import "./index.css";
+import Card from './view/myMuisc';
+import NavBottom from './components/navBottom';
+import Fllow from "./view/fllow";
+import Download from "./view/download";
+import './style/comm.less'
 
 export default function App() {
   return (
@@ -13,7 +15,9 @@ export default function App() {
       <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/home" exact element={<Home />}></Route>
-            <Route path="/card" element={<Card />}></Route>
+            <Route path="/my" element={<Card />}></Route>
+            <Route path="/friend" element={<Fllow />}></Route>
+            <Route path="/download" element={<Download />}></Route>
         </Routes>
     </Router>
   );
---
+++ src/view/download/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Download extends React.Component {
+    render() {
+        return (
+            <div>下载页</div>
+        )
+    }
+}
+
+export default Download;
